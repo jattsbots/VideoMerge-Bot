ad307c351109117f431dc8392918d3b6ea483c6b
Anmol Singh <bxjjjzid@gmail.com>
2024-11-17 21:29:25 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -4,20 +4,20 @@ import os
 
 
 class Config(object):
-    API_ID = os.environ.get("API_ID")
-    API_HASH = os.environ.get("API_HASH")
+    API_ID = os.environ.get("API_ID", 20841292)
+    API_HASH = os.environ.get("API_HASH", 58c4e84b9b2506684df6447d3e324dd9)
     BOT_TOKEN = os.environ.get("BOT_TOKEN")
     SESSION_NAME = os.environ.get("SESSION_NAME", "Video-Merge-Bot")
     UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL")
-    LOG_CHANNEL = os.environ.get("LOG_CHANNEL")
+    LOG_CHANNEL = os.environ.get("LOG_CHANNEL", -1002198681571)
     DOWN_PATH = os.environ.get("DOWN_PATH", "./downloads")
-    TIME_GAP = int(os.environ.get("TIME_GAP", 5))
-    MAX_VIDEOS = int(os.environ.get("MAX_VIDEOS", 5))
+    TIME_GAP = int(os.environ.get("TIME_GAP", 2))
+    MAX_VIDEOS = int(os.environ.get("MAX_VIDEOS", 10))
     STREAMTAPE_API_USERNAME = os.environ.get("STREAMTAPE_API_USERNAME")
     STREAMTAPE_API_PASS = os.environ.get("STREAMTAPE_API_PASS")
-    MONGODB_URI = os.environ.get("MONGODB_URI")
+    MONGODB_URI = os.environ.get("MONGODB_URI", mongodb+srv://renamerbot0:anmol@cluster0.mxaxukp.mongodb.net/)
     BROADCAST_AS_COPY = bool(os.environ.get("BROADCAST_AS_COPY", False))
-    BOT_OWNER = int(os.environ.get("BOT_OWNER", 1445283714))
+    BOT_OWNER = int(os.environ.get("BOT_OWNER", 6052303737))
 
     START_TEXT = """
 Hi Unkil, I am Video Merge Bot!
